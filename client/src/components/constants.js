4d029d841feae7fe930b028c330662c65765e49f
priaynshu9548 <priaynshusisodiya7088@gmail.com>
2024-01-04 17:50:19 +0530

    graphs

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,3 +1,3 @@
-export const URL = "https://dashboard2-sjkd.onrender.com/api/v1";
+export const URL = "http://localhost:5000/api/v1";
 export const BASE_URL = "http://localhost:5000/api/v1";
 //export const BASE_URL = "http://localhost:5000/api/v1";
