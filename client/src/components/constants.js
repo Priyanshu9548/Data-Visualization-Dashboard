a9a5bce85c377b367eec385796c827cc4a5a196c
priaynshu9548 <priaynshusisodiya7088@gmail.com>
2024-01-04 18:19:32 +0530

    graphs

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,3 +1,4 @@
-export const URL = "https://dashboardvis.onrender.com//api/v1";
 export const BASE_URL = "http://localhost:5000/api/v1";
+export const URL = "http://localhost:5000/api/v1";
+//export const URL = "https://dashboardvis.onrender.com/api/v1";
 //export const BASE_URL = "http://localhost:5000/api/v1";
