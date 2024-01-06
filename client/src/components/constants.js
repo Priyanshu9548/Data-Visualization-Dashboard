451a3362c39185d04f833865f2c35e9792f72b6e
Priyanshu Sisodiya <78598054+Priyanshu9548@users.noreply.github.com>
2024-01-06 12:42:52 +0530

    Update constants.js

--- client/src/components/constants.js
+++ client/src/components/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:5000/api/v1";
-export const URL = process.env.RENDER_URL;
-//export const URL = "https://dashboardvis.onrender.com/api/v1";
+// export const URL = process.env.RENDER_URL;
+export const URL = "https://dashboardvis.onrender.com/api/v1";
 //export const BASE_URL = "http://localhost:5000/api/v1";
